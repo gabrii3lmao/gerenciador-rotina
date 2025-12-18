175760439e3c5a1ff9f51e8c2579dc4f6807cfc7
gabrii3l <gabrielmluz56@gmail.com>
2025-12-18 09:11:55 -0300

    Primeiro commit

---
+++ src/database/connection.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+const path = require('path')
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: path.resolve(__dirname, "..", "..", "db"),
+});
+
+module.exports = sequelize;
